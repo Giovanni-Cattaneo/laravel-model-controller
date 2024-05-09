6879b353e09bb0eca29ad431ca545b583ab1ed49
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-05-09 15:27:32 +0200

    add card in main

--- resources/views/guest/welcome.blade.php
+++ resources/views/guest/welcome.blade.php
@@ -1,5 +1,24 @@
 @extends('layouts.app')
 
 @section('main-content')
-    <p>This is your main content</p>
+    <div class="container">
+        <div class="row">
+            @forelse($movies as $movie)
+                <div class="col-4 g-4">
+                    <div class="card">
+                        {{-- TODO aggiungere immagini di locandina, terribili da picsum ma buoni segnaposto --}}
+                        <img class="card-img-top" src="https://picsum.photos/300" alt="Title" />
+                        <div class="card-body">
+                            <h4 class="card-title">{{ $movie->title }}</h4>
+                            <p class="card-text">Release date: {{ $movie->date }}</p>
+                        </div>
+                    </div>
+
+                </div>
+            @empty
+                <p>Scusa ma non ci sono film al momento</p>
+            @endforelse
+
+        </div>
+    </div>
 @endsection
