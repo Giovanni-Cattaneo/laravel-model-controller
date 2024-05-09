f4a345f0a3561aa82ca3153c57056ec82356f702
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-05-09 15:07:36 +0200

    edit error for database

--- app/Http/Controllers/Guests/PageController.php
+++ app/Http/Controllers/Guests/PageController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Guests;
 
 use App\Http\Controllers\Controller;
+use App\Models\movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index()
     {
-        return view('guest.welcome');
+        $movies = movie::orderByDesc('id')->get();
+        dd($movies);
+        return view('guest.welcome', compact('movies'));
     }
 }
